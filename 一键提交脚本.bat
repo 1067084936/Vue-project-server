ca6aba2be6354da2c51bababeeb6b8bb0ef0b58e
dzh <1067084936@qq.com>
2020-10-28 17:41:10 +0800

    2020/10/28 17:40:57 backup from windows, comments: 第一次提交

---
+++ 一键提交脚本.bat
@@ -0,0 +1,14 @@
+@each off
+set d=%date:~0,10%
+set t=%time:~0,8%
+set timestamp=%d% %t%
+
+set /p comments=please input commit comments:
+
+git status
+git add .
+git commit -m "%timestamp% backup from windows, comments: %comments%"
+git push origin master
+echo "提交成功"
+git log --stat -1
+pause
